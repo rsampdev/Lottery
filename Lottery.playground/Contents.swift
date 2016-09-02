fb6db46fd8677c2cf697895fb752265300f43345
rsampdev <rsampdev@gmail.com>
2016-09-02 14:47:45 -0400

    Fixed userOnlyGuessedPowerball and fixed precision loss bug on lottery jackpot

--- Lottery.playground/Contents.swift
+++ Lottery.playground/Contents.swift
@@ -4,33 +4,33 @@ typealias LotteryBalls = (ballOne: Int, ballTwo: Int, ballThree: Int, ballFour: 
 
 func play(userGuess: LotteryBalls) -> Int  {
     let random = GKRandomDistribution.init(lowestValue: 0, highestValue: 99)
-    let lotteryJackpot = (random.nextInt() * random.nextInt() * random.nextInt() * random.nextInt() * random.nextInt() * random.nextInt()) + 10000
+    let tenthOfLotteryJackpot = GKRandomDistribution.init(lowestValue: 10000, highestValue: 1000000000000).nextInt()
     let lotteryBalls: LotteryBalls = (random.nextInt(), random.nextInt(), random.nextInt(), random.nextInt(), random.nextInt(), random.nextInt())
-    let userOnlyGuessedThePowerball = (userGuess.0 == lotteryBalls.0 && userGuess.1 == lotteryBalls.1 && userGuess.2 == lotteryBalls.2 && userGuess.3 == lotteryBalls.3 && userGuess.4 == lotteryBalls.4 && userGuess.5 == lotteryBalls.5);
+    let userOnlyGuessedThePowerball = (userGuess.0 != lotteryBalls.0 && userGuess.1 != lotteryBalls.1 && userGuess.2 != lotteryBalls.2 && userGuess.3 != lotteryBalls.3 && userGuess.4 != lotteryBalls.4 && userGuess.5 == lotteryBalls.5);
     
     var userWinnings = 0
     
     if userGuess == lotteryBalls {
-        userWinnings = lotteryJackpot
+        userWinnings = tenthOfLotteryJackpot
     } else {
         if userGuess.ballOne == lotteryBalls.ballOne {
-            userWinnings += lotteryJackpot / 10
+            userWinnings += tenthOfLotteryJackpot
         }
         if userGuess.ballTwo == lotteryBalls.ballTwo {
-            userWinnings += lotteryJackpot / 10
+            userWinnings += tenthOfLotteryJackpot
         }
         if userGuess.ballThree == lotteryBalls.ballThree {
-            userWinnings += lotteryJackpot / 10
+            userWinnings += tenthOfLotteryJackpot
         }
         if userGuess.ballFour == lotteryBalls.ballFour {
-            userWinnings += lotteryJackpot / 10
+            userWinnings += tenthOfLotteryJackpot
         }
         if userGuess.ballFive == lotteryBalls.ballFive {
-            userWinnings += lotteryJackpot / 10
+            userWinnings += tenthOfLotteryJackpot
         }
         
         if userOnlyGuessedThePowerball {
-            userWinnings += lotteryJackpot / 10
+            userWinnings += tenthOfLotteryJackpot / 10
         } else if userGuess.powerball == lotteryBalls.powerball {
             userWinnings *= 2
         }
